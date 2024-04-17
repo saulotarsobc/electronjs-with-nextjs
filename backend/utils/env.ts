22f8a6cce5f7e319e9afebc72e01cf0eaeafb550
saulotarsobc <saulotarsobc@gmail.com>
2024-04-17 09:06:44 -0300

    :fire:

--- backend/utils/env.ts
+++
@@ -1,2 +0,0 @@
-export const isDev = process.argv.some((str) => str == "--dev");
-export const isStart = process.argv.some((str) => str == "--start");
--- backend/utils/index.ts
+++
@@ -1,6 +0,0 @@
-import { app } from "electron";
-import { join } from "path";
-
-// "file:./database.sqlite"
-export const databaseUrl =
-  "file:" + join(app.getPath("userData"), "database.sqlite");
